9839a0f0ec899a063853062b9f89b7cfb23453be
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2018-01-22 13:32:22 +0300

    init

---
+++ PaketBug34/Program.fs
@@ -0,0 +1,9 @@
+﻿open Akka.Cluster.Discovery
+open Akkling
+
+[<EntryPoint>]
+let main _ = 
+    let system = System.create "test" (Configuration.load())
+    ClusterDiscovery.Join system |> ignore
+    system.WhenTerminated.Wait()
+    0
